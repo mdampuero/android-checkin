8cc66bf3e8994438c6af84743b30ba3240aa8279
Mauricio Ampuero <mauricio.ampuero@openhost.es>
2025-11-11 20:44:48 +0100

    Migration enpoint

--- app/src/main/java/cl/clickgroup/checkin/fragments/CheckInFragment.kt
+++ app/src/main/java/cl/clickgroup/checkin/fragments/CheckInFragment.kt
@@ -9,7 +9,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import retrofit2.Call
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.ProgressBar
